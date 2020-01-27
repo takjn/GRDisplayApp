0c8200a173c8258014f8a97d3177ca4ee60a53e2
Jun Takeda <takeda.jun@gmail.com>
2020-01-28 00:34:55 +0900

    fix: add main.go

---
+++ cmd/displayapp/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"time"
+
+	"github.com/takjn/GRDisplayApp/pkg/parser"
+	"go.bug.st/serial"
+	"go.bug.st/serial.v1/enumerator"
+)
+
+// the device VID and PID
+const (
+	vid = "1f00"
+	pid = "2012"
+)
+
+func findDevice() (string, error) {
+	ports, err := enumerator.GetDetailedPortsList()
+	if err != nil {
+		return "", err
+	}
+	if len(ports) == 0 {
+		return "", fmt.Errorf("no serial ports found")
+	}
+	for _, port := range ports {
+		if port.IsUSB && port.VID == vid && port.PID == pid {
+			return port.Name, nil
+		}
+	}
+
+	return "", fmt.Errorf("no device found")
+}
+
+func main() {
+	var path, filename string
+	var interval int
+	flag.StringVar(&path, "p", "", "output path")
+	flag.StringVar(&filename, "f", "test", "filename")
+	flag.IntVar(&interval, "i", 1000, "jpeg interval (ms)")
+	flag.Parse()
+
+	binaryMode := true
+	if path != "" {
+		binaryMode = false
+		filename = filename + "%d.jpg"
+		path = filepath.Join(path, filename)
+	}
+
+	// Find a Mbed CDC &serial.Mode{}
+	name, err := findDevice()
+	if err != nil {
+		log.Fatalf("failed to find device: %v", err)
+		os.Exit(1)
+	}
+
+	// Open the CDC &serial.Mode{}
+	port, err := serial.Open(name, &serial.Mode{})
+	if err != nil {
+		log.Fatalf("failed to open device: %v", err)
+		os.Exit(1)
+	}
+	defer port.Close()
+
+	go func() {
+		i := 0
+		for {
+			data, err := parser.GetData(port)
+			if err != nil {
+				return
+			}
+			if binaryMode {
+				os.Stdout.Write(data)
+			} else {
+				f := fmt.Sprintf(path, i)
+				err = ioutil.WriteFile(f, data, 0666)
+				if err != nil {
+					log.Fatalf("failed to write file: %v", err)
+					break
+				}
+				log.Println(f, ",", len(data))
+				time.Sleep(time.Millisecond * time.Duration(interval))
+				i++
+			}
+		}
+	}()
+
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt)
+	<-sig
+}
